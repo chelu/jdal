5a7d589af5d68ab48cad95f992bd1ae795f83627
Jose Luis Martin <jlm@joseluismartin.info>
2012-04-13 12:49:52 +0200

    Add <jdal:criteria> element for manage criteraBuilders and update ServiceBeanDefinitionParser to handle it.

--- core/src/main/java/info/joseluismartin/beans/ServiceBeanDefinitionParser.java
+++ core/src/main/java/info/joseluismartin/beans/ServiceBeanDefinitionParser.java
@@ -15,17 +15,22 @@
  */
 package info.joseluismartin.beans;
 
+
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.config.BeanReference;
 import org.springframework.beans.factory.config.ConstructorArgumentValues;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.parsing.BeanComponentDefinition;
 import org.springframework.beans.factory.parsing.CompositeComponentDefinition;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.beans.factory.support.ManagedMap;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * Parse jdal:service element and register a {@link info.joseluismartin.dao.Dao} and 
@@ -38,13 +43,17 @@ import org.w3c.dom.Element;
  */
 public class ServiceBeanDefinitionParser implements BeanDefinitionParser {
 	
-	private static String DAO_SUFFIX = "Dao";
-	private static String SERVICE_SUFFIX = "Service";
-	private static String JPA_DAO_CLASS_NAME = "info.joseluismartin.dao.jpa.JpaDao";
-	private static String ENTITY = "entity";
-	private static String DAO_CLASS = "daoClass";
-	private static String SERVICE_CLASS = "serviceClass";
-	private static String PERSISTENT_SERVICE_CLASS_NAME = "info.joseluismartin.logic.PersistentManager";
+	private static final String DAO_SUFFIX = "Dao";
+	private static final String SERVICE_SUFFIX = "Service";
+	private static final String JPA_DAO_CLASS_NAME = "info.joseluismartin.dao.jpa.JpaDao";
+	private static final String ENTITY = "entity";
+	private static final String DAO_CLASS = "daoClass";
+	private static final String SERVICE_CLASS = "serviceClass";
+	private static final String PERSISTENT_SERVICE_CLASS_NAME = "info.joseluismartin.logic.PersistentManager";
+	private static final String NAME = "name";
+	private static final String BUILDER = "builder";
+	private static final String CRITERIA = "criteria";
+	private static final String CRITERIA_BUILDER_MAP = "criteriaBuilderMap";
 
 	/**
 	 * {@inheritDoc}
@@ -67,6 +76,17 @@ public class ServiceBeanDefinitionParser implements BeanDefinitionParser {
 			String name = StringUtils.uncapitalize(
 					StringUtils.substringAfterLast(className, PropertyUtils.PROPERTY_SEPARATOR));
 		
+			// DAO
+			BeanDefinitionBuilder daoBuilder  = BeanDefinitionBuilder.genericBeanDefinition(daoClassName);
+			NodeList nl = element.getElementsByTagNameNS(element.getNamespaceURI(), CRITERIA);
+			if (nl.getLength() > 0) {
+				ManagedMap<String, BeanReference> builders = new ManagedMap<String, BeanReference>(nl.getLength());
+				for (int i = 0; i < nl.getLength(); i++) {
+					Element e = (Element) nl.item(i);
+					builders.put(e.getAttribute(NAME), new RuntimeBeanReference(e.getAttribute(BUILDER)));
+				}
+				daoBuilder.addPropertyValue(CRITERIA_BUILDER_MAP, builders);
+			}
 			GenericBeanDefinition dao =  new GenericBeanDefinition();
 			dao.setBeanClassName(daoClassName);
 			ConstructorArgumentValues cav = new ConstructorArgumentValues();
