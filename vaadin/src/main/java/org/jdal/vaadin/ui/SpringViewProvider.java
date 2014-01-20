9149d8c706a8d2cebdcc4bf01cdf924f00d3a446
Jose Luis Martin <jlm@joseluismartin.info>
2014-01-20 21:47:03 +0100

    Add SpringViewProvider and ComponentViewAdapter for use with Vaadin Navigator.
    - SpringViewProvider: Lookup views by name in bean factory.
    - ComponentViewAdapter: generic adapter to make components Views.

---
+++ vaadin/src/main/java/org/jdal/vaadin/ui/SpringViewProvider.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2009-2014 Jose Luis Martin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jdal.vaadin.ui;
+
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewProvider;
+import com.vaadin.ui.Component;
+
+/**
+ * ViewProvider that delegates on a Spring BeanFactory to find Views
+ * 
+ * @author Jose Luis Martin
+ * @since 2.0
+ */
+public class SpringViewProvider implements ViewProvider {
+	
+	@Autowired
+	private BeanFactory beanFactory;
+
+	@Override
+	public String getViewName(String viewAndParameters) {
+		return viewAndParameters;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public View getView(String viewName) {
+		Object view  =  beanFactory.getBean(viewName);
+		
+		Component c = null;
+		
+		if (view instanceof org.jdal.vaadin.ui.VaadinView) {
+			c = ((VaadinView) view).getPanel();
+		}
+		else if (view instanceof Component) {
+			c= (Component) view;
+		}
+		
+		return (View) (c instanceof View ? c : new ComponentViewAdapter(c));
+	}
+
+	/**
+	 * @return the beanFactory
+	 */
+	public BeanFactory getBeanFactory() {
+		return beanFactory;
+	}
+
+	/**
+	 * @param beanFactory the beanFactory to set
+	 */
+	public void setBeanFactory(BeanFactory beanFactory) {
+		this.beanFactory = beanFactory;
+	}
+
+
+}
